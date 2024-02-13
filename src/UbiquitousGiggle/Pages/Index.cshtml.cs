30203d0b44612c59a25c6ce7c8ecacf30af67822
Dmitrii Sosedov <dsosedov@vmware.com>
2024-02-13 16:36:59 -0500

    WIP

--- src/UbiquitousGiggle/Pages/Index.cshtml.cs
+++ src/UbiquitousGiggle/Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ubiquitous_giggle.Pages;
+namespace UbiquitousGiggle.Pages;
 
 public class IndexModel : PageModel
 {
---
+++ test/UbiquitousGiggle.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace UbiquitousGiggle.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
